6156c6d232457617142ab56fbb848fbbf1d6d868
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-09-13 20:33:14 +0300

    Add files via upload

---
+++ 01_Syntax, Functions and Statements/Lab/10-aggregate-elements.js
@@ -0,0 +1,25 @@
+function solve(input){
+    let elements = Array.from(input);
+
+    function sum(array){
+        return array.reduce((a, x) => a + x, 0);
+    }
+
+    function inversedSum(array){
+        return array.reduce((a, x) => a + 1 / x, 0) ;
+    }
+
+    function concat(array){
+        return array.join('');
+    }
+
+    let elementsSum = sum(elements);
+    let inversedElementsSum = inversedSum(elements);
+    let contactedElements = concat(elements);
+
+    console.log(elementsSum);
+    console.log(inversedElementsSum); 
+    console.log(contactedElements);
+}
+
+solve([1, 2, 3]);
