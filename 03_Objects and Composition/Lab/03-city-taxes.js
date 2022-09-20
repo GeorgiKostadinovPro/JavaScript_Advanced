cb9f6aa743d1c94c2e75b2b8075877daffbb8bf8
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-09-20 18:04:58 +0300

    100 / 100

---
+++ 03_Objects and Composition/Lab/03-city-taxes.js
@@ -0,0 +1,20 @@
+function cityTaxes(name, population, treasury){
+    return {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes(){
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage){
+            this.population += this.population * percentage / 100;
+        },
+        applyRecession(percentage){
+            this.treasury -= this.treasury * percentage / 100;
+        }
+    }
+}
+
+const city = cityTaxes('Tortuga', 7000, 15000);
+console.log(city);
