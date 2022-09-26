55dd07ce99d19a9db77132f7b939c79dab936fdf
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-09-26 20:33:55 +0300

    Delete 05_DOM Manipulations and Events/Lab directory

--- 05_DOM Manipulations and Events/Lab/01. List-Of-Items/app.js
+++
@@ -1,9 +0,0 @@
-function addItem() {
-    let textInputElement = document.getElementById('newItemText');
-    let ulElement = document.getElementById('items');
-
-    let liElement = document.createElement('li');
-    liElement.textContent = textInputElement.value;
-
-    ulElement.appendChild(liElement);
-}
