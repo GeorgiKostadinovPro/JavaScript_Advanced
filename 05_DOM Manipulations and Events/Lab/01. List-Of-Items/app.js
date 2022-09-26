96ccc2b1126a5a0ca547e307428d2fee7947a7bf
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-09-26 17:13:49 +0300

    01 List-Of-Items, 02. Delete-From-Table 100 / 100

---
+++ 05_DOM Manipulations and Events/Lab/01. List-Of-Items/app.js
@@ -0,0 +1,9 @@
+function addItem() {
+    let textInputElement = document.getElementById('newItemText');
+    let ulElement = document.getElementById('items');
+
+    let liElement = document.createElement('li');
+    liElement.textContent = textInputElement.value;
+
+    ulElement.appendChild(liElement);
+}
