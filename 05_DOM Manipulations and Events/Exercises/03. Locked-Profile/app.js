397ca40b1b32c64a1d6611274d62075e26d53fd7
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-09-27 20:30:45 +0300

    100 / 100

---
+++ 05_DOM Manipulations and Events/Exercises/03. Locked-Profile/app.js
@@ -0,0 +1,24 @@
+function lockedProfile() {
+    let showMoreButtonElements = Array.from(document.getElementsByTagName('button'));
+
+    showMoreButtonElements.forEach(el => {
+        el.addEventListener('click', (e) => {
+            let currProfileDivElement = e.currentTarget.parentNode;
+            let lockInputRadioElement = currProfileDivElement.querySelector('input[value="lock"]');
+
+            if(lockInputRadioElement.checked){
+                return;
+            }
+            
+            let hiddenInformationDivElement = currProfileDivElement.querySelector('div');
+
+            if(e.currentTarget.textContent === 'Show more'){
+                hiddenInformationDivElement.style.display = 'inline-block';
+                e.currentTarget.textContent = 'Hide it'; 
+            }else{
+                hiddenInformationDivElement.style.display = 'none';
+                e.currentTarget.textContent = 'Show more'; 
+            }
+        });
+    });
+}
